895f51190124b24f3934d544b9764dc269aceecc
Michael <mnath10@stuy.edu>
2020-07-28 18:28:07 -0400

    able to extract information from add task form; going to add to db now

--- routes/addtasks.js
+++ routes/addtasks.js
@@ -1,14 +1,21 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const db = require("../models");
+router.get("/", async (req, res) => {
+  const myTasks = await db.Tasks.findAll();
+  console.log(myTasks);
+  res.render("adder", { name: "Monkeys Moo", tasks: myTasks });
+});
 
-router.get('/', (req, res) => {
+router.post("/api", (req, res) => {
+  // retrieving information from the form that user fills out
+  const newTask = req.body;
+  const newTaskName = newTask["taskName"];
+  const newTaskDescription = newTask["taskDescription"];
+  const newTaskDate = newTask["taskDate"];
 
-    res.send("Monkey Moo")
-})
-
-router.post("/", (req, res) => {
-    console.log(req.body)
-    res.send("Request Received")
+  console.log([newTaskName, newTaskDescription, newTaskDate]);
+  res.send("Request Received");
 });
 
 module.exports = router;
